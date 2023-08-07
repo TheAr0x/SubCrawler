facf2e3768a5efc412032ace31de697d7bdb76eb
Ar0x <Thear0x@protonmail.com>
2023-08-07 20:09:50 +0200

    Rename and relocate the wide list of subdomains

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ print_banner()
 print("Made with love by Ar0x\n")
 target_url = input("Enter an URL (format : site.com) : ")
 
-with open("./suddom_list.txt", 'r') as subdom_list:
+with open("assets/wide_list.txt", 'r') as subdom_list:
     for line in subdom_list:
         word = line.strip()
         test_url = word + "." + target_url
